0aa7001fe18378764155fa481c2ef01b1666cbd2
matiferamador <107516034+matiferamador@users.noreply.github.com>
2023-12-14 20:01:07 -0300

    guardar cambiois

--- wp-content/themes/mfatheme/functions.php
+++ wp-content/themes/mfatheme/functions.php
@@ -23,14 +23,12 @@ function mfa_scripts_styles() {
     wp_enqueue_style('normalize', 'https://necolas.github.io/normalize.css/latest/normalize.css', array(), '8.0.1');
     wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', array(), '11.0.5');
     wp_enqueue_style('flowbite-css', 'https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css', array(), '2.2.0');
-    wp_enqueue_style('aos-css', 'https://unpkg.com/aos@2.3.1/dist/aos.css', array(), '2.3.1'); // Agrega la línea para AOS
-    wp_enqueue_style('style', get_stylesheet_uri(), array('normalize', 'swiper-css', 'aos-css'), '1.0.0');
+    wp_enqueue_style('style', get_stylesheet_uri(), array('normalize', 'swiper-css'), '1.0.0');
 
     // Archivos scripts
     wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), '11.0.5');
     wp_enqueue_script('flowbite-js', 'https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js', array(), '2.2.0');
-    wp_enqueue_script('aos-js', 'https://unpkg.com/aos@2.3.1/dist/aos.js', array('jquery'), '2.3.1', true); // Corrige el cuarto parámetro
-    wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('swiper-js', 'flowbite-js', 'aos-js'), '1.0.0');
+    wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('swiper-js', 'flowbite-js'), '1.0.0');
 }
 
 add_action('wp_enqueue_scripts', 'mfa_scripts_styles');
--- wp-content/themes/mfatheme/js/scripts.js
+++ wp-content/themes/mfatheme/js/scripts.js
@@ -1,7 +1,4 @@
-
-
 document.addEventListener("DOMContentLoaded", function () {
-  AOS.init();
   // Verifica si el elemento con la clase .mySwiper existe
   if (document.querySelector('.mySwiper')) {
     var swiper = new Swiper(".mySwiper", {
